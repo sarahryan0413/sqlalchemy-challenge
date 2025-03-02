9a924fbeaeaa1baf8e003885f5ab10e4c0207448
Sarah Ryan <sarahryan0413@gmail.com>
2025-03-02 11:37:46 -0600

    almost finished with flask - still figuring out start/end time

--- Starter_Code/app.py
+++ Starter_Code/app.py
@@ -80,9 +80,60 @@ def stations():
 
     # Unravel (using np.ravel) results into a 1D array and convert to a list
     station_results = list(np.ravel(stations))
-    return jsonify(station_results)
+    return jsonify(stations=station_results)
 
+
+# Create a temperature observations route
+@app.route("/api/v1.0/tobs")
+def tobs():
+    """Retrieves dates and temperature observations from the most-active station (USC00519281) for the past year"""
+    # Calculate the date one year from the last date in data set.
+    year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    
+    # Perform a query to retrieve the data and precipitation scores
+    query_results = session.query(Measurement.tobs)\
+                              .filter(Measurement.station == 'USC00519281')\
+                              .filter(Measurement.date >= year_ago).all()
+    session.close()
+
+    # Unravel (using np.ravel) results into a 1D array and convert to a list
+    most_active_station_results = list(np.ravel(query_results))
+    return jsonify(temperatures=most_active_station_results)
+
+
+# Create a route for min, max, and avg temps before a specified start or start-end range
+@app.route("/api/v1.0/start_date")
+@app.route("/api/v1.0/start_date/end_date")
+def temp_stats(start, end=None):     #start is mandatory, end=None means its not required
+    """Return temperature min, max, and avg"""
+    
+    # select statement - creates a list of SQL functions to get min, max, and avg
+    sel = [func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)]
+
+    if not end: 
+        start = dt.datetime.strptime(start, "%m-%d-%y")    #converts start time string to a datetime object
+        
+        #calculate temp min, max, and avg for dates greater than start
+        results = session.query(*sel).filter(Measurement.date>=start).all()
+        session.close()
+
+        #unravel results into a 1D array and convert to a list
+        temps=list(np.ravel(results))
+        return jsonify(temps)
+
+    #calculate temp min,max, and avg with start and stop
+    start = dt.datetime.strptime(start, "%m-%d-%y")
+    end = dt.datetime.strptime(end, "%m-%d-%y")
+
+    results = session.query(*sel).filter(Measurement.date>=start)\
+                                 .filter(Measurement.date<=end).all()
+    session.close()
+
+    # Unravel (using np.ravel) results into a 1D array and convert to a list
+    temps = list(np.ravel(results))
+    return jsonify(temperatures=temps)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
